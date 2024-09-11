7c7c448c6e600e0bf4fe36a5dc440208d6bfede8
Mikael Wallin <mikael.x.wallin@gmail.com>
2024-09-11 22:51:11 +0200

    added styles

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,15 +1,22 @@
-import PasswordAtom  from "./PasswordAtom";
+import PasswordAtom from "./PasswordAtom";
 import UserAtom from "./UserAtom";
 import LogintButtonAtom from "./LoginButtonAtom";
+import styles from "./Login.module.css";
 
 const Login = () => {
-    return(
-        <>
-            <UserAtom />
-            <PasswordAtom />
-            <LogintButtonAtom title='Login'/>
-        </>
-    )
-}
+    return (
+        <div className={styles.container}>
+            <div className={styles.inputField}>
+                <UserAtom />
+            </div>
+            <div className={styles.inputField}>
+                <PasswordAtom />
+            </div>
+            <div className={styles.button}>
+                <LogintButtonAtom title='Login' />
+            </div>
+        </div>
+    );
+};
 
 export default Login;
